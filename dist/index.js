228c78ec4d97331ed009b4c15c9c88ee8127cf92
Andrej Sarafimovski <andrejsarafimovski@gmail.com>
2022-08-02 00:27:55 +0200

    test

--- dist/index.js
+++ dist/index.js
@@ -46271,7 +46271,7 @@ const fs = __nccwpck_require__(7147);
 function lintSchemas(schemas) {
     // We have to join '..' since we are located at dist/index.js
     console.log(fs.readdirSync(__dirname));
-    console.log(fs.readdirSync(path.join('..', __dirname)));
+    console.log(fs.readdirSync(path.join(__dirname, '..')));
     schemas = schemas.map(schema => path.join('..', schema))
     const args = [
         null,
