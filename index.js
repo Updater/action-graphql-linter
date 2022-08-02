0ae7d1657bd983d1e5e72389289a64d6fd92c92d
Andrej Sarafimovski <andrejsarafimovski@gmail.com>
2022-08-02 12:03:44 +0200

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const { runner } = require('graphql-schema-linter');
+const fs = require('fs');
+const path = require('path');
 
 function lintSchemas(schemas) {
     const args = [
@@ -8,12 +9,12 @@ function lintSchemas(schemas) {
         __dirname,
         ...schemas
     ];
+    console.log(fs.readFileSync(path.join(__dirname, schemas[0])).toString());
     console.log('Linting schemas: ', schemas.join())
     return runner.run(process.stdout, process.stdin, process.stderr, args)
 }
 
 async function main() {
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(process.argv)
     try {
         const schemaFiles = core.getInput('files').split(',');
